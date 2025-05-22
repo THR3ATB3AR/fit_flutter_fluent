a2aaa8f94f16a8aeef446f26ff031791a1e3ecd6
THR3ATB3AR <threatbevr@gmail.com>
2025-05-22 21:58:29 +0200

    deleted tooltip

--- lib/widgets/repack_item.dart
+++ lib/widgets/repack_item.dart
@@ -8,57 +8,21 @@ class RepackItem extends StatelessWidget {
 
   const RepackItem({super.key, required this.repack, required this.itemHeight});
 
-  String _overflowText(String text, int maxLength) {
-    return text.length > maxLength
-        ? '${text.substring(0, maxLength)}...'
-        : text;
-  }
-
   @override
   Widget build(BuildContext context) {
-    return Tooltip(
-        verticalOffset: 120,
-        waitDuration: const Duration(milliseconds: 1000),
-        exitDuration: const Duration(milliseconds: 0),
-        richMessage: TextSpan(
-          text: '${_overflowText(repack.title, 30)}\n',
-          style: const TextStyle(fontWeight: FontWeight.bold),
-          children: [
-            TextSpan(
-                text:
-                    '{AppLocalizations.of(context)!.genres}: ${repack.genres}\n',
-                children: [
-                  TextSpan(
-                    text:
-                        '{AppLocalizations.of(context)!.company}: ${repack.company}\n',
-                  ),
-                  TextSpan(
-                    text:
-                        '{AppLocalizations.of(context)!.language}: ${repack.language}\n',
-                  ),
-                  TextSpan(
-                    text:
-                        '{AppLocalizations.of(context)!.originalSize}: ${repack.originalSize}\n',
-                  ),
-                  TextSpan(
-                      text:
-                          '{AppLocalizations.of(context)!.repackSize}: ${repack.repackSize}'),
-                ]),
-          ],
+    return Card(
+      margin: const EdgeInsets.all(8.0),
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(8.0),
+      ),
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(8.0),
+        child: CachedNetworkImage(
+          imageUrl: repack.cover,
+          fit: BoxFit.cover,
+          width: itemHeight * 0.65,
         ),
-        child: Card(
-          margin: const EdgeInsets.all(8.0),
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(8.0),
-          ),
-          child: ClipRRect(
-            borderRadius: BorderRadius.circular(8.0),
-            child: CachedNetworkImage(
-              imageUrl: repack.cover,
-              fit: BoxFit.cover,
-              width: itemHeight * 0.65,
-            ),
-          ),
-        ));
+      ),
+    );
   }
 }
